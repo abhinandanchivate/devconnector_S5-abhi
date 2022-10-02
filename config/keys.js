82dbbfa2b4b2aba48c2bf7927d5d69fc476ebdaa
abhinandanchivate <abhinandanchivate2000@gmail.com>
2022-10-02 11:58:06 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://RameshMango:admin@ac-wbo7ytr-shard-00-00.fih6be1.mongodb.net:27017,ac-wbo7ytr-shard-00-01.fih6be1.mongodb.net:27017,ac-wbo7ytr-shard-00-02.fih6be1.mongodb.net:27017/devconnector?ssl=true&replicaSet=atlas-pq48aa-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
